db644dad6f1c47ed3c387738ba3e49d50e588266
Daniel-Packer <packer.61@osu.edu>
2023-11-14 11:06:24 -0500

    some more exploratory

---
+++ src/exploratory.py
@@ -0,0 +1,27 @@
+
+coffee_strength_key = {
+  "Weak" : 1,
+  "Somewhat light" : 2,
+  "Medium": 3,
+  "Somewhat strong" : 4,
+  "Very Strong": 5}
+
+age_ordering = [
+  "<18 years old",
+  "18-24 years old",
+  "25-34 years old",
+  "35-44 years old",
+  "45-54 years old",
+  "55-64 years old",
+  ">65 years old",
+]
+
+coffee_spending_ordering = [
+  "Less than $20",
+  "$20-$50",
+  "$50-$100",
+  "$100-$300",
+  "$300-$500",
+  "$500-$1000",
+  "More than $1,000",
+]
